60a426d33ff8f5ebbbe4c8a35dc734fbe31a97e8
Neil Farmer <neilfarmer@Neils-MacBook-Pro.local>
2025-05-04 15:48:57 -0400

    feat: Add go-git version flag

--- cmd/root.go
+++ cmd/root.go
@@ -1,9 +1,11 @@
 package cmd
 
 import (
+	"fmt"
 	"log/slog"
 	"os"
 
+	"github.com/neilfarmer/go-git/internal"
 	"github.com/spf13/cobra"
 )
 
@@ -12,13 +14,8 @@ var verboseLevel string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "go-git",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Go-git is a command line tool to clone and manage repositories",
+	Long: `Go-git is a command line tool to clone and manage repositories from different source control management systems (SCM) like GitHub, GitLab, etc.`,
 
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 		var level slog.Level
@@ -41,9 +38,15 @@ to quickly create a Cobra application.`,
 	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	// Couldnt figure out how to do this the cobra way
+	for _, arg := range os.Args[1:] {
+		if arg == "-V" || arg == "--version" {
+			fmt.Println("go-git version:", internal.Version)
+			os.Exit(0)
+		}
+	}
+
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
@@ -51,15 +54,6 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.go-git.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().BoolP("version", "V", false, "Print version and exit")
 }
-
-
